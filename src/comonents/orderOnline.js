bddd937e9a7385017f7c745cbd09a136745ef142
unknown <saradawsone@gmail.com>
2023-02-10 14:56:23 -0700

    wip

--- src/comonents/orderOnline.js
+++ src/comonents/orderOnline.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css'
 
 const initialInfo = {
     firstName: '',
@@ -11,9 +13,31 @@ function OrderOnline() {
         <div className='orderOnline-container'>
             <form>
                 <label>First Name</label>
-                <input type='text' name='firstName' />
+                <input 
+                    type='text' 
+                    name='firstName' 
+                    placeholder='Enter first name'
+                    min='4'
+                    max='36'
+                    required/>
+
                 <label>Last Name</label>
-                <input type='text' name='firstName' />          
+                <input 
+                    type='text' 
+                    name='lastName' 
+                    placeholder='Enter last name'
+                    min='1'
+                    max='36'
+                    required/>
+
+                <label>Phone Number</label>
+                <input 
+                    type="tel"  
+                    name="phone"
+                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    required/>
+
+                <small>Format: 123-456-7890</small>          
             </form>
         </div>
     )
