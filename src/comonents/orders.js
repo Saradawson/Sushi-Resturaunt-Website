47a9e8e3f00f8e4a4d3a35393d1ba15a8b1c8dc0
unknown <saradawsone@gmail.com>
2023-02-20 17:20:13 -0700

    order state passing along

--- src/comonents/orders.js
+++ src/comonents/orders.js
@@ -4,7 +4,13 @@ const Orders = (props) => {
     const { orders } = props
     return(
         <div className='orders-container'>
-            {}
+            {orders.map((order, idx) => (
+                <div key={idx} className='order-container'>
+                    <h3>{order.firstName}</h3>
+                    <h6>{order.order}</h6>
+                    <h6>{`$${order.total}.00`}</h6>
+                </div>
+            ))}
         </div>
     )
 }
